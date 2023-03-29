ff5fecd8ea207d41e2af523f2e9b57bda842a2dd
swaroop <akasamjyothiswaroop93@gmail.com>
2023-03-29 17:09:28 +0530

    first commit

---
+++ market/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market2.db'
+app.config['SECRET_KEY'] = 'bd3aafa24250412a3ebbbc51'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+from market import routes
